08cf18c1b42af4224e670d07270ce95fe2fc9c85
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-07-13 12:09:55 +0200

    villages alpha-0.0.7-test code

--- src/main/java/cz/maxtechnik/old_villages/worldgen/OldVillagePieces.java
+++ src/main/java/cz/maxtechnik/old_villages/worldgen/OldVillagePieces.java
@@ -19,17 +19,17 @@ import org.jetbrains.annotations.NotNull;
 public class OldVillagePieces {
 
 	public static class VillagePiece extends StructurePiece {
-		private final int pieceType; // 0 = Studna, 1 = Malý dům, 2 = Velký dům...
+		private final int pieceType; // 0 = Studna, 1 = Cesta, 2 = Malý dům
 
-		// Konstruktor pro vytváření nové hry za běhu
-		public VillagePiece(int pieceType, int genDepth, int x, int y, int z, Direction orientation) {
-			// Voláme super s typem, hloubkou generování a boxem vytvořeným pomocí orientBox
-			super(OldVillagesMod.OLD_VILLAGE_PIECE.get(), genDepth, BoundingBox.orientBox(x, y, z, 0, 0, 0, 6, 12, 6, orientation));
+		// Konstruktor pro vytváření kousků za běhu hry
+		public VillagePiece(int pieceType, int genDepth, int x, int y, int z, int sizeX, int sizeY, int sizeZ, Direction orientation) {
+			// Velikost boxu se teď předává dynamicky podle typu budovy
+			super(OldVillagesMod.OLD_VILLAGE_PIECE.get(), genDepth, BoundingBox.orientBox(x, y, z, 0, 0, 0, sizeX, sizeY, sizeZ, orientation));
 			this.pieceType = pieceType;
 			this.setOrientation(orientation);
 		}
 
-		// Konstruktor pro načítání ze souboru světa (NBT) - Anotace potlačí varování o nepoužitém 'context'
+		// Konstruktor pro načítání ze souboru světa (NBT)
 		@SuppressWarnings("unused")
 		public VillagePiece(StructurePieceSerializationContext context, CompoundTag tag) {
 			super(OldVillagesMod.OLD_VILLAGE_PIECE.get(), tag);
@@ -41,19 +41,42 @@ public class OldVillagePieces {
 			tag.putInt("PieceType", this.pieceType);
 		}
 
-		// ====================================================================
-		// SAMOTNÉ PROCEDURÁLNÍ STAVĚNÍ BLOKŮ
-		// ====================================================================
 		@Override
 		public void postProcess(@NotNull WorldGenLevel level, @NotNull StructureManager structureManager, @NotNull ChunkGenerator generator, @NotNull RandomSource random, @NotNull BoundingBox box, @NotNull ChunkPos chunkPos, @NotNull BlockPos startPos) {
-			// Stavba klasické STARÉ STUDNY (Well) z verze 1.7.10
-			if(this.pieceType == 0){
-				BlockState cobble = Blocks.COBBLESTONE.defaultBlockState();
-				BlockState oakFence = Blocks.OAK_FENCE.defaultBlockState();
-				BlockState water = Blocks.WATER.defaultBlockState();
+			BlockState cobble = Blocks.COBBLESTONE.defaultBlockState();
+			BlockState oakFence = Blocks.OAK_FENCE.defaultBlockState();
+			BlockState water = Blocks.WATER.defaultBlockState();
+			BlockState planks = Blocks.OAK_PLANKS.defaultBlockState();
+			BlockState log = Blocks.OAK_LOG.defaultBlockState();
+			BlockState gravel = Blocks.GRAVEL.defaultBlockState();
 
-				// 1. Kompletní stavba stěn bazénku od Y=0 do Y=3 (voda už nevyteče bokem)
-				for (int y = 0; y <= 3; y++) {
+			// ================================================================
+			// TYP 0: KLASTRICKÁ STUDNA S ŠIROKÝM PRSTENCEM (6x6)
+			// ================================================================
+			if (this.pieceType == 0) {
+				// 1. Široký spodní prstenec platformy (Y=0) od 0 do 5
+				for (int x = 0; x <= 5; x++) {
+					for (int z = 0; z <= 5; z++) {
+						this.placeBlock(level, cobble, x, 0, z, box);
+
+						// Pilíře dolů pod celou platformou 6x6, aby nelétala v krajině
+						for (int y = -1; y >= -40; y--) {
+							BlockState underBlock = this.getBlock(level, x, y, z, box);
+							if (underBlock.isAir() || underBlock.is(Blocks.WATER) || underBlock.is(Blocks.LAVA)
+									|| underBlock.is(Blocks.SHORT_GRASS) || underBlock.is(Blocks.TALL_GRASS)
+									|| underBlock.is(Blocks.SNOW) || underBlock.is(Blocks.SPRUCE_LEAVES)
+									|| underBlock.is(Blocks.OAK_LEAVES) || underBlock.is(Blocks.SPRUCE_LOG)
+									|| underBlock.is(Blocks.OAK_LOG)) {
+								this.placeBlock(level, cobble, x, y, z, box);
+							} else {
+								break;
+							}
+						}
+					}
+				}
+
+				// 2. Vyvýšené stěny samotné studny (Y=1 až Y=3) posunuté doprostřed (1 až 4)
+				for (int y = 1; y <= 3; y++) {
 					for (int i = 1; i <= 4; ++i) {
 						this.placeBlock(level, cobble, i, y, 1, box);
 						this.placeBlock(level, cobble, i, y, 4, box);
@@ -62,44 +85,20 @@ public class OldVillagePieces {
 					}
 				}
 
-				// 2. Cobblestone dno uvnitř studny na Y=0
-				this.placeBlock(level, cobble, 2, 0, 2, box);
-				this.placeBlock(level, cobble, 3, 0, 2, box);
-				this.placeBlock(level, cobble, 2, 0, 3, box);
-				this.placeBlock(level, cobble, 3, 0, 3, box);
+				// 3. Vnitřní dno (Y=1) a naplnění vodou (Y=2 a Y=3)
+				this.placeBlock(level, cobble, 2, 1, 2, box);
+				this.placeBlock(level, cobble, 3, 1, 2, box);
+				this.placeBlock(level, cobble, 2, 1, 3, box);
+				this.placeBlock(level, cobble, 3, 1, 3, box);
 
-				// 3. Naplnění vnitřku vodou na Y=1 a Y=2
-				for (int y = 1; y <= 2; y++) {
+				for (int y = 2; y <= 3; y++) {
 					this.placeBlock(level, water, 2, y, 2, box);
 					this.placeBlock(level, water, 3, y, 2, box);
 					this.placeBlock(level, water, 2, y, 3, box);
 					this.placeBlock(level, water, 3, y, 3, box);
 				}
 
-				// 4. FIX PROTI LÉTÁNÍ: Pilíře pod stěnami studny klesající dolů
-				for (int x = 1; x <= 4; ++x) {
-					for (int z = 1; z <= 4; ++z) {
-						// Pilíře stavíme pouze pod obvodovými stěnami studny
-						if (x == 1 || x == 4 || z == 1 || z == 4) {
-							for (int y = -1; y >= -40; y--) {
-								BlockState underBlock = this.getBlock(level, x, y, z, box);
-
-								// Ignorujeme vzduch, vodu, trávu, sníh i listy/kmeny stromů, aby pilíř prošel až na pevnou hlínu/kámen
-								if (underBlock.isAir() || underBlock.is(Blocks.WATER) || underBlock.is(Blocks.LAVA)
-										|| underBlock.is(Blocks.SHORT_GRASS) || underBlock.is(Blocks.TALL_GRASS)
-										|| underBlock.is(Blocks.SNOW) || underBlock.is(Blocks.SPRUCE_LEAVES)
-										|| underBlock.is(Blocks.OAK_LEAVES) || underBlock.is(Blocks.SPRUCE_LOG)
-										|| underBlock.is(Blocks.OAK_LOG)) {
-									this.placeBlock(level, cobble, x, y, z, box);
-								} else {
-									break; // Narazili jsme na solidní podklad (hlína, stone) -> pilíř je ukotven
-								}
-							}
-						}
-					}
-				}
-
-				// 5. Rohové ploty držící střechu (Y=4 a Y=5)
+				// 4. Rohové ploty držící střechu (Y=4 a Y=5)
 				this.placeBlock(level, oakFence, 1, 4, 1, box);
 				this.placeBlock(level, oakFence, 1, 4, 4, box);
 				this.placeBlock(level, oakFence, 4, 4, 1, box);
@@ -109,14 +108,60 @@ public class OldVillagePieces {
 				this.placeBlock(level, oakFence, 4, 5, 1, box);
 				this.placeBlock(level, oakFence, 4, 5, 4, box);
 
-				// 6. Cobblestone střecha na Y=6
+				// 5. Cobblestone střecha (Y=6) posunutá na rozměr 4x4 nad studnou
 				for (int x = 1; x <= 4; ++x) {
 					for (int z = 1; z <= 4; ++z) {
 						this.placeBlock(level, cobble, x, 6, z, box);
 					}
 				}
 			}
-			// Sem pak v budoucnu jednoduše přidáš další `else if (this.pieceType == 1)` pro Malý dům...
+
+			// ================================================================
+			// TYP 1: STARÁ ŠTĚRKOVÁ CESTA (Šířka 3, Délka dynamická)
+			// ================================================================
+			else if (this.pieceType == 1) {
+				// Bounding Box cesty (vyplníme povrch štěrkem na Y=0)
+				for (int x = 0; x < this.getGrid().getXSize(); x++) {
+					for (int z = 0; z < this.getGrid().getZSize(); z++) {
+						this.placeBlock(level, gravel, x, 0, z, box);
+						this.placeBlock(level, Blocks.AIR.defaultBlockState(), x, 1, z, box);
+						this.placeBlock(level, Blocks.AIR.defaultBlockState(), x, 2, z, box);
+					}
+				}
+			}
+
+			// ================================================================
+			// TYP 2: KLASICKÝ MALÝ DŮMEK (Cobble základy, dřevěné stěny)
+			// ================================================================
+			else if (this.pieceType == 2) {
+				// Vyčistíme vnitřní prostor domu vzduchem
+				this.fillWithBlocks(level, box, 1, 1, 1, 4, 4, 4, Blocks.AIR.defaultBlockState(), Blocks.AIR.defaultBlockState(), false);
+
+				// Cobblestone podlaha a základy (Y=0)
+				this.fillWithBlocks(level, box, 0, 0, 0, 5, 0, 5, cobble, cobble, false);
+
+				// Stěny domu (Y=1 až Y=3)
+				this.fillWithBlocks(level, box, 0, 1, 0, 5, 3, 0, planks, planks, false);
+				this.fillWithBlocks(level, box, 0, 1, 5, 5, 3, 5, planks, planks, false);
+				this.fillWithBlocks(level, box, 0, 1, 1, 0, 3, 4, planks, planks, false);
+				this.fillWithBlocks(level, box, 5, 1, 1, 5, 3, 4, planks, planks, false);
+
+				// Rohové dřevěné sloupy (Logy)
+				for(int y=1; y<=3; y++) {
+					this.placeBlock(level, log, 0, y, 0, box);
+					this.placeBlock(level, log, 5, y, 0, box);
+					this.placeBlock(level, log, 0, y, 5, box);
+					this.placeBlock(level, log, 5, y, 5, box);
+				}
+
+				// Plochá cobblestone střecha (Y=4)
+				this.fillWithBlocks(level, box, 0, 4, 0, 5, 4, 5, cobble, cobble, false);
+
+				// Dummy okna a dveře
+				this.placeBlock(level, Blocks.GLASS_PANE.defaultBlockState(), 2, 2, 0, box);
+				this.placeBlock(level, Blocks.AIR.defaultBlockState(), 2, 1, 5, box); // Vchod ze zadní strany cesty
+				this.placeBlock(level, Blocks.AIR.defaultBlockState(), 2, 2, 5, box);
+			}
 		}
 	}
 }
--- src/main/java/cz/maxtechnik/old_villages/worldgen/OldVillageStructure.java
+++ src/main/java/cz/maxtechnik/old_villages/worldgen/OldVillageStructure.java
@@ -7,35 +7,35 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
-public class OldVillageStructure extends Structure{
-	// Každá moderní struktura musí mít svůj Codec
-	public static final MapCodec<OldVillageStructure> CODEC=RecordCodecBuilder.mapCodec(instance->
-			instance.group(settingsCodec(instance)).apply(instance,OldVillageStructure::new));
-	public OldVillageStructure(StructureSettings settings){
+
+public class OldVillageStructure extends Structure {
+	public static final MapCodec<OldVillageStructure> CODEC = RecordCodecBuilder.mapCodec(instance ->
+			instance.group(settingsCodec(instance)).apply(instance, OldVillageStructure::new));
+
+	public OldVillageStructure(StructureSettings settings) {
 		super(settings);
 	}
+
 	@Override
-	protected @NotNull Optional<GenerationStub> findGenerationPoint(GenerationContext context){
-		ChunkPos chunkPos=context.chunkPos();
+	protected @NotNull Optional<GenerationStub> findGenerationPoint(GenerationContext context) {
+		ChunkPos chunkPos = context.chunkPos();
 		int blockX = chunkPos.getMinBlockX() + 8;
 		int blockZ = chunkPos.getMinBlockZ() + 8;
 
-		// Zjistíme výšku terénu pod stromy
 		int height = context.chunkGenerator().getFirstOccupiedHeight(
-				blockX,
-				blockZ,
-				Heightmap.Types.OCEAN_FLOOR_WG, // ZMĚNA: WORLD_SURFACE_WG změněno na OCEAN_FLOOR_WG
+				blockX, blockZ,
+				Heightmap.Types.OCEAN_FLOOR_WG,
 				context.heightAccessor(),
 				context.randomState()
 		);
 
-		// FIX PRO SUPERFLAT: Pokud výšková mapa vrátí nesmysl nebo nulu, vynutíme minimální bezpečnou výšku terénu
 		if (height <= context.heightAccessor().getMinBuildHeight()) {
 			height = context.chunkGenerator().getFirstFreeHeight(blockX, blockZ, Heightmap.Types.WORLD_SURFACE, context.heightAccessor(), context.randomState());
 		}
@@ -43,18 +43,82 @@ public class OldVillageStructure extends Structure{
 		BlockPos startPos = new BlockPos(blockX, height, blockZ);
 		return Optional.of(new GenerationStub(startPos, (builder) -> generatePieces(builder, context, startPos)));
 	}
-	private void generatePieces(StructurePiecesBuilder builder,GenerationContext context,BlockPos pos){
-		Direction randomDirection=Direction.Plane.HORIZONTAL.getRandomDirection(context.random());
-		// PŘIDÁNO: Přidali jsme hloubku generování '0' jako druhý parametr
-		builder.addPiece(new OldVillagePieces.VillagePiece(
-				0, // pieceType (0 = studna)
-				0, // genDepth (startovní hloubka generování)
-				pos.getX(),pos.getY(),pos.getZ(),
-				randomDirection
-		));
+
+	// ====================================================================
+	// PROCEDURÁLNÍ LOGIKA SKLÁDÁNÍ VESNICE V JAVĚ
+	// ====================================================================
+	private void generatePieces(StructurePiecesBuilder builder, GenerationContext context, BlockPos pos) {
+		// 1. Postavíme hlavní startovní studnu (Velikost 6x6x7)
+		Direction mainDirection = Direction.Plane.HORIZONTAL.getRandomDirection(context.random());
+		builder.addPiece(new OldVillagePieces.VillagePiece(0, 0, pos.getX(), pos.getY(), pos.getZ(), 6, 7, 6, mainDirection));
+
+		// 2. Vyvrtáme cesty do VŠECH 4 světových stran od studny!
+		Direction[] directions = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
+
+		for (Direction dir : directions) {
+			// Vypočítáme startovní pozici cesty na okraji studny
+			int startX = pos.getX();
+			int startZ = pos.getZ();
+
+			// Posuneme začátek cesty k hraně studny podle směru
+			if (dir == Direction.NORTH) startZ -= 1;
+			else if (dir == Direction.SOUTH) startZ += 6;
+			else if (dir == Direction.WEST) startX -= 1;
+			else if (dir == Direction.EAST) startX += 6;
+
+			int pathLength = 16; // Délka cesty v blocích
+			int pathWidth = 3;   // Šířka cesty
+
+			// Vytvoříme pomocný zkušební Bounding Box pro cestu
+			BoundingBox checkPathBox = BoundingBox.orientBox(startX, pos.getY(), startZ, 0, 0, 0, pathWidth, 1, pathLength, dir);
+
+			// Pokud cesta nekoliduje s ničím, co už stojí, zapíšeme ji!
+			if (builder.nextIntersecting(checkPathBox) == null) {
+				builder.addPiece(new OldVillagePieces.VillagePiece(1, 1, startX, pos.getY(), startZ, pathWidth, 1, pathLength, dir));
+
+				// 3. SKLÁDÁNÍ DOMŮ PODÉL TÉTO CESTY
+				// Projedeme osu délky cesty a pokusíme se každých pár bloků postavit domek
+				for (int offsetZ = 2; offsetZ < pathLength - 6; offsetZ += 7) {
+
+					// Zkusíme dům vlevo od cesty (Direction.WEST vzhledem k ose cesty)
+					tryPlaceHouse(builder, startX, pos.getY(), startZ, offsetZ, -6, dir.getCounterClockWise(), dir, 1);
+
+					// Zkusíme dům vpravo od cesty (Direction.EAST vzhledem k ose cesty)
+					tryPlaceHouse(builder, startX, pos.getY(), startZ, offsetZ, pathWidth + 1, dir.getClockWise(), dir, 1);
+				}
+			}
+		}
 	}
+
+	/**
+	 * Pomocná metoda, která propočítá pozici domu, zkontroluje kolize a umístí ho.
+	 */
+	private static void tryPlaceHouse(StructurePiecesBuilder builder, int pathX, int pathY, int pathZ, int offsetZ, int offsetX, Direction houseFacing, Direction pathDir, int depth) {
+		// Výpočet absolutní pozice ve světě podle směru cesty a offsetů
+		int houseX = pathX + (pathDir.getStepX() * offsetZ) + (pathDir.getClockWise().getStepX() * offsetX);
+		int houseZ = pathZ + (pathDir.getStepZ() * offsetZ) + (pathDir.getClockWise().getStepZ() * offsetX);
+
+		int sizeX = 6;
+		int sizeY = 6;
+		int sizeZ = 6;
+
+		// Vytvoříme zkušební krabici pro dům
+		BoundingBox houseBox = BoundingBox.orientBox(houseX, pathY, houseZ, 0, 0, 0, sizeX, sizeY, sizeZ, houseFacing);
+
+		// KONTROLA KOLIZÍ: Pokud dům nevráží do studny, cesty ani jiného domu, bezpečně ho vygenerujeme!
+		if (builder.nextIntersecting(houseBox) == null) {
+			builder.addPiece(new OldVillagePieces.VillagePiece(
+					2,          // pieceType 2 = Malý dům
+					depth + 1,  // hloubka generování
+					houseX, pathY, houseZ,
+					sizeX, sizeY, sizeZ,
+					houseFacing // dům bude otočen čelem k cestě
+			));
+		}
+	}
+
 	@Override
-	public @NotNull StructureType<?> type(){
+	public @NotNull StructureType<?> type() {
 		return OldVillagesMod.OLD_VILLAGE.get();
 	}
 }
